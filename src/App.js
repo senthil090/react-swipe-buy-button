a1f6738c38af6cba89d0b4454be05add9b974650
SENTHILKUMAR S <senthil24jun@gmail.com>
2020-08-08 14:50:39 +0530

    reset button added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import SwipeButton from "./react-swipe-button";
 var swipesize = "60";
-var oncomplete = function () {
-  window.navigator && window.navigator.vibrate(200);
-};
 var reset = React.createRef();
-var resetbutton = function () {
-  reset.current && reset.current.reset();
-};
+
 export default function App() {
+  var isreset = useState(false);
+  var oncomplete = function () {
+    window.navigator &&
+      window.navigator.vibrate &&
+      window.navigator.vibrate(200);
+    isreset[1](true);
+  };
+  var resetbutton = function () {
+    reset.current && reset.current.reset();
+    isreset[1](false);
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>
-        Start editing to see some magic happen!{" "}
-        <button onClick={resetbutton}>Reset</button>
-      </h2>
+      <h2>Start editing to see some magic happen! </h2>
       <div className="swipebuy">
         <SwipeButton
           size={swipesize}
-          info="Swipe to buy"
+          info="Swipe to proceed"
           oncomplete={oncomplete}
           ref={reset}
         />
       </div>
+      {isreset[0] && (
+        <div className="resetbutton">
+          <button onClick={resetbutton}>Reset </button>
+        </div>
+      )}
     </div>
   );
 }
